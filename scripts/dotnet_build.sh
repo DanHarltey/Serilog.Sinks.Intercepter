563e8f240e3cb72196c843862ed63312897abeb5
Dan H <dan.r.hartley@gmail.com>
2023-02-23 22:08:02 +0000

    missed save file

--- scripts/dotnet_build.sh
+++ scripts/dotnet_build.sh
@@ -2,5 +2,6 @@
 set -e
 
 dotnet restore ../
-dotnet build ../ --configuration Release --nologo --no-restore
-dotnet test ../ --configuration Release --nologo --no-build --verbosity normal
+dotnet build ../ --configuration Release --no-restore
+dotnet test ../ --configuration Release --no-build --verbosity normal
+dotnet pack ../ --configuration Release --no-build
