7e2e94cfc8af645187f76a914fc40f2d15049970
Dan H <dan.r.hartley@gmail.com>
2023-02-26 21:39:25 +0000

    smaller faster

---
+++ performance/Serilog.Sinks.Intercepter.Benchmarks/Program.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using Serilog.Sinks.Intercepter.Intercepters;
+using Serilog.Events;
+
+namespace Serilog.Sinks.Intercepter.Benchmarks;
+
+[MemoryDiagnoser]
+public class Program
+{
+    static void Main(string[] args)
+    {
+        _ = BenchmarkRunner.Run(typeof(Program));
+    }
+    
+    private IIntercepter? _intercepter;
+    private IIntercepter? _intercepter1;
+
+    private LogEvent? _errorLogEvent;
+
+
+    [GlobalSetup]
+    public void SetUp()
+    {
+        _intercepter = new LogLevelBufferIntercepter(LogEventLevel.Error);
+        _intercepter1 = new LogLevelBufferIntercepter1(LogEventLevel.Error);
+        _errorLogEvent = GetLogEvent(LogEventLevel.Error);
+    }
+
+
+    [Benchmark(Baseline = true)]
+    public object? Array()
+    {
+        object? obj = null;
+
+        var events = _intercepter!.Process(_errorLogEvent!);
+
+        foreach (var item in events)
+        {
+            obj = item;
+        }
+        return obj;
+    }
+
+    [Benchmark()]
+    public object? Singal()
+    {
+        object? obj = null;
+
+        var events = _intercepter1!.Process(_errorLogEvent!);
+
+        foreach (var item in events)
+        {
+            obj = item;
+        }
+        return obj;
+    }
+
+    private static LogEvent GetLogEvent(LogEventLevel logLevel = LogEventLevel.Debug) => new(
+        DateTimeOffset.UtcNow,
+        logLevel,
+        null,
+        MessageTemplate.Empty,
+        Enumerable.Empty<LogEventProperty>());
+}
---
+++ src/Serilog.Sinks.Intercepter/Single.cs
@@ -0,0 +1,41 @@
+﻿using Serilog.Events;
+using System.Collections;
+
+namespace Serilog.Sinks.Intercepter;
+
+internal class Single : IEnumerable<LogEvent>
+{
+    private readonly LogEvent _logEvent;
+
+    public Single(LogEvent logEvent) =>
+        _logEvent = logEvent;
+
+    public IEnumerator<LogEvent> GetEnumerator() => new SingleEnumerator(_logEvent);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private class SingleEnumerator : IEnumerator<LogEvent>
+    {
+        private readonly LogEvent _logEvent;
+        private ulong _index;
+
+
+        public SingleEnumerator(LogEvent logEvent)
+        {
+            _logEvent = logEvent;
+            Reset();
+        }
+
+        public LogEvent Current => _index == 1 ? _logEvent : default!;
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext() => _index++ == 0;
+
+        public void Reset() => _index =0;
+    }
+}
