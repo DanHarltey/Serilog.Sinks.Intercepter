e8d9d33da7bd23d2f7537511f70dd9226db06750
Dan H <dan.r.hartley@gmail.com>
2023-07-22 22:00:52 +0100

    Adding code coverage report

--- scripts/dotnet_build.sh
+++ scripts/dotnet_build.sh
@@ -3,5 +3,5 @@ set -e
 
 dotnet restore ../
 dotnet build ../ --configuration Release --no-restore
-dotnet test ../ --configuration Release --no-build --verbosity normal
+dotnet test ../ --configuration Release --no-build --verbosity normal --collect:"XPlat Code Coverage;Format=lcov"
 dotnet pack ../ --configuration Release --no-build
