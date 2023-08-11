b01435274d5fd047ad59e34f5eb62a824a89c4c6
Dan H <dan.r.hartley@gmail.com>
2023-08-11 22:17:41 +0100

    Nuget package improvements Deterministic Builds, source link, symbol package (.snupkg)

--- scripts/dotnet_build.sh
+++ scripts/dotnet_build.sh
@@ -2,6 +2,6 @@
 set -e
 
 dotnet restore ../
-dotnet build ../ --configuration Release --no-restore
+dotnet build ../ --configuration Release --no-restore /p:ContinuousIntegrationBuild=true
 dotnet test ../ --configuration Release --no-build --verbosity normal --collect:"XPlat Code Coverage;Format=lcov"
 dotnet pack ../ --configuration Release --no-build
