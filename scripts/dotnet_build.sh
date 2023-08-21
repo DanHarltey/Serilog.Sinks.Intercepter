cb8d2e19ef448033468f5e585ba130dd055d1cdb
Dan H <dan.r.hartley@gmail.com>
2023-08-21 06:50:35 +0100

    Adding native ARM64 test run

--- scripts/dotnet_build.sh
+++ scripts/dotnet_build.sh
@@ -3,5 +3,11 @@ set -e
 
 dotnet restore ../
 dotnet build ../ --configuration Release --no-restore /p:ContinuousIntegrationBuild=true
-dotnet test ../ --configuration Release --no-build --verbosity normal --collect:"XPlat Code Coverage;Format=lcov"
+
+if [[ $1 = "code_coverage" ]]; then
+  dotnet test ../ --configuration Release --no-build --verbosity normal --collect:"XPlat Code Coverage;Format=lcov"
+else
+  dotnet test ../ --configuration Release --no-build --verbosity normal
+fi
+
 dotnet pack ../ --configuration Release --no-build
